45614893e961d4cb94254ceccc80044574713f9c
Gestalt Representative <gestaltrep@gmail.com>
2026-03-10 15:08:10 -0400

    install twilio sdk

--- app/api/sms-webhook/route.ts
+++ app/api/sms-webhook/route.ts
@@ -3,18 +3,14 @@ export async function POST() {
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Message>
-Welcome to Flags.
-
-Complete check-in here:
+Welcome. Please complete event check-in here:
 https://flags-platform.vercel.app/checkin
   </Message>
 </Response>`;
 
   return new Response(twiml, {
     status: 200,
-    headers: {
-      "Content-Type": "text/xml"
-    }
+    headers: { "Content-Type": "text/xml" }
   });
 
 }
