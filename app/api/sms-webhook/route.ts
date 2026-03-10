166d835e1c05b10426b720e55c9bffb496baca85
Gestalt Representative <gestaltrep@gmail.com>
2026-03-10 14:49:58 -0400

    fix twilio webhook

--- app/api/sms-webhook/route.ts
+++ app/api/sms-webhook/route.ts
@@ -1,18 +1,20 @@
-export async function POST(req: Request) {
+export async function POST() {
 
-  const params = await req.text();
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Message>
+Welcome to Flags.
 
-  const response = `
-  <Response>
-    <Message>
-      Welcome to Flags. Complete check-in here:
-      https://flags-platform.vercel.app/checkin
-    </Message>
-  </Response>
-  `;
+Complete check-in here:
+https://flags-platform.vercel.app/checkin
+  </Message>
+</Response>`;
 
-  return new Response(response, {
-    headers: { "Content-Type": "text/xml" }
+  return new Response(twiml, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/xml"
+    }
   });
 
 }
