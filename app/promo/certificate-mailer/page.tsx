5bb611dbe667fa4b1cc29bed0e122f72858f939c
Gestalt Representative <gestaltrep@gmail.com>
2026-04-14 04:37:35 -0400

    fix: match vertical padding to horizontal

--- app/promo/certificate-mailer/page.tsx
+++ app/promo/certificate-mailer/page.tsx
@@ -63,9 +63,9 @@ function RoundedBox() {
         style={{
           position: "absolute",
           left: 232,
-          top: 71,
+          top: 69,
           width: 331,
-          height: 118,
+          height: 123,
           background: "#fff",
           zIndex: 1,
         }}
