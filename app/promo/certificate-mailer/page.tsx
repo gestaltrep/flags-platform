11c587fdfed4efc988773e80bbc2cd073c726200
Gestalt Representative <gestaltrep@gmail.com>
2026-04-14 05:04:12 -0400

    fix: space R from AVE

--- app/promo/certificate-mailer/page.tsx
+++ app/promo/certificate-mailer/page.tsx
@@ -84,7 +84,7 @@ function RoundedBox() {
           zIndex: 2,
         }}
       >
-        <span style={{ WebkitTextStroke: "3px #222", color: "#fff", fontWeight: 700, marginRight: 5 }}>R</span>
+        <span style={{ WebkitTextStroke: "3px #222", color: "#fff", fontWeight: 700, marginRight: 8 }}>R</span>
         <span style={{ WebkitTextStroke: "3px #222", color: "#fff", fontWeight: 700, marginRight: -11 }}>A</span>
         <span style={{ marginRight: 3, fontSize: 114 }}>V</span>
         <span style={{ fontSize: 114 }}>E</span>
