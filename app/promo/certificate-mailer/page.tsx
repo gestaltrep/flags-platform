1faba33c49feaf13532a2a7fb020e2f99014cf81
Gestalt Representative <gestaltrep@gmail.com>
2026-04-14 04:49:47 -0400

    fix: tighter vertical cutout

--- app/promo/certificate-mailer/page.tsx
+++ app/promo/certificate-mailer/page.tsx
@@ -63,9 +63,9 @@ function RoundedBox() {
         style={{
           position: "absolute",
           left: 232,
-          top: 77,
+          top: 83,
           width: 331,
-          height: 107,
+          height: 95,
           background: "#fff",
           zIndex: 1,
         }}
