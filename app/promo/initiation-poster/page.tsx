ebcf1a450c18f68e0f8738bd11fb97981cca87b0
Gestalt Representative <gestaltrep@gmail.com>
2026-04-14 06:43:44 -0400

    Revert "fix: scale up Bass Tab to align with Signo tip"
    This reverts commit efeccb3b02e921f1a257fdad020c51a9b2a64255.

--- app/promo/initiation-poster/page.tsx
+++ app/promo/initiation-poster/page.tsx
@@ -229,8 +229,8 @@ export default function InitiationPosterPage() {
                 src="/BASS TABERNACLE.png"
                 alt="Bass Tabernacle"
                 style={{
-                  width: 135,
-                  height: 135,
+                  width: 110,
+                  height: 110,
                   objectFit: "contain",
                   marginLeft: -35,
                 }}
