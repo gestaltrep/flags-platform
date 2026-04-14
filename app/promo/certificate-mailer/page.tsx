d55547ff9b8c34f468b15c90996f7f3fa1e3edb4
Gestalt Representative <gestaltrep@gmail.com>
2026-04-14 04:40:25 -0400

    fix: tight vertical cutout around RAVE

--- app/promo/certificate-mailer/page.tsx
+++ app/promo/certificate-mailer/page.tsx
@@ -63,9 +63,9 @@ function RoundedBox() {
         style={{
           position: "absolute",
           left: 232,
-          top: 71,
+          top: 74,
           width: 331,
-          height: 118,
+          height: 113,
           background: "#fff",
           zIndex: 1,
         }}
